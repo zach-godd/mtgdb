916da4db4f08d871c497c1fc771080abf03e50ae
Zachary Goddard <zath0492@gmail.com>
2026-07-06 21:35:00 -0500

    refacor in prog

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"mtgdb/api"
+	"mtgdb/data"
 )
 
 func main() {
+	a := api.NewAPI(data.ScryfallClient{})
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/bulk-data", api.HandleFetchBulkData)
+	mux.HandleFunc("/api/bulk-data", a.HandleFetchBulkData)
 
 	log.Println("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
